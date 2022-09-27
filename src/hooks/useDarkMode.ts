a97527327e47d4bbce655bb229347c2888d26b63
Chris Serra <789723+cbserra@users.noreply.github.com>
2022-09-27 07:20:24 -0700

    feat(dark-mode): 💄 copied over dark-mode hook and connected it

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import useLocalStorage from 'use-local-storage'
+
+export const useDarkMode = () => {
+  const isSystemInDarkMode = matchMedia('(prefers-color-scheme: dark)')
+  const prefersDarkMode = () => isSystemInDarkMode.matches
+
+  const [lsDarkModeEnabled, setLsDarkModeEnabled] = useLocalStorage<boolean>(
+    'dark-mode',
+    prefersDarkMode()
+  )
+  const [darkMode, toggleDarkMode] = useState<boolean>(lsDarkModeEnabled)
+
+  useEffect(() => {
+    setLsDarkModeEnabled(darkMode)
+
+    if (window === undefined) return
+    const root = window.document.documentElement
+
+    if (darkMode) {
+      root.classList.add('dark')
+    } else {
+      root.classList.remove('dark')
+    }
+  }, [darkMode, setLsDarkModeEnabled])
+
+  return { darkMode, toggleDarkMode }
+}
