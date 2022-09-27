62e9c67fde4e5c287fb2d86aa62310720250354f
Chris Serra <789723+cbserra@users.noreply.github.com>
2022-09-27 07:17:51 -0700

    build(configs): 🔧 update config files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,14 @@
 
 module.exports = {
   content: ['./*.html', './src/**/*.{js,ts,jsx,tsx,css}'],
+  darkMode: 'class',
   theme: {
-
-    extend: {},
+    extend: {
+      borderRadius: {
+        'input-radius': '1.5rem',
+        'button-radius': '1.0rem'
+      }
+    },
     fontFamily: {
       mono: ['Space Mono', 'monospace']
     },
@@ -20,16 +25,29 @@ module.exports = {
       'off-white': '#FEFEFE',
       'ghost-white': '#F6F8FF',
       'dark-gunmetal': '#141D2F',
-      'yankees-blue': '#1E2A47'
+      'yankees-blue': '#1E2A47',
+      'dark-toggle-hover': '#90A4D4',
+      'button-hover': '#60ABFF',
+      'avatar-outline-green': '#4BB543',
     },
     fontSize: {
       base: '62.5%',
-      body: '1.5rem 2.5rem',
-      toggle: ['1.3rem', {
-        lineHeight: '1.9rem',
-        letterSpacing: '0.25rem',
-        fontWeight: '700'
-      }],
+      detail: ['1.3rem', { lineHeight: '2.2rem' }],
+
+      'stats-label': ['1.1rem', { lineHeight: '1.6rem' }],
+      'detail-lg': ['1.5rem', { lineHeight: '2.2rem' }],
+      body: ['1.3rem', { lineHeight: '2.5rem' }],
+      'body-lg': ['1.5rem', { lineHeight: '2.5rem' }],
+      links: ['1.3rem', { lineHeight: '1.9rem' }],
+      'links-lg': ['1.6rem', { lineHeight: '2.4rem' }],
+      toggle: [
+        '1.3rem',
+        {
+          lineHeight: '1.9rem',
+          letterSpacing: '0.25rem',
+          fontWeight: '700'
+        }
+      ],
       'heading-1': [
         '2.6rem',
         {
@@ -41,18 +59,21 @@ module.exports = {
       'heading-3': ['1.6rem', { lineHeight: '2.4rem', fontWeight: '400' }],
       'heading-4': ['1.3rem', { lineHeight: '2.0rem', fontWeight: '400' }],
       'input-sm': ['1.3rem', { lineHeight: '2.5rem', fontWeight: '400' }],
-      'button-sm': ['1.4rem', { lineHeight: '2.1rem', fontWeight: '700' }],
+      'button-sm': ['1.4rem', { lineHeight: '2.1rem', fontWeight: '700' }]
     },
     boxShadow: {
-      'input-shadow': '0px 16px 30px -10px rgba(70, 96, 187, 0.198567)'
+      section: '0px 16px 30px -10px rgba(70, 96, 187, 0.198567)'
     },
-    borderRadius: {
-      'input-radius': '1.5rem',
-      'button-radius': '1.0rem'
-    },
+
     fontWeight: {
       normal: '400',
       bold: '700'
+    },
+    backgroundImage: {
+      'icon-company': "url('./assets/images/icon-company.svg')",
+      'icon-location': "url('./assets/images/icon-location.svg')",
+      'icon-website': "url('./assets/images/icon-website.svg')",
+      'icon-twitter': "url('./assets/images/icon-twitter.svg')"
     }
   },
   plugins: []
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import viteImagemin from 'vite-plugin-imagemin'
+import svgr from 'vite-plugin-svgr'
 
 import config from './config.js'
 import postcss from './postcss.config.js'
@@ -13,8 +14,9 @@ const { imagemin } = config
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    svgr(),
     react(),
-    viteImagemin(imagemin),
+    // viteImagemin(imagemin),
     createHtmlPlugin({
       minify: true,
       entry: 'src/main.tsx'
@@ -22,12 +24,12 @@ export default defineConfig({
   ],
   css: {
     postcss
-  },
-  resolve: {
-    alias: [
-      { find: '@/', replacement: '/src' },
-      { find: '@/Assets', replacement: '/src/assets' },
-      { find: '@/Components', replacement: '/src/components' }
-    ]
   }
+  // resolve: {
+  //   alias: [
+  //     { find: '@/', replacement: '/src' },
+  //     { find: '@/Assets', replacement: '/src/assets' },
+  //     { find: '@/Components', replacement: '/src/components' }
+  //   ]
+  // }
 })
